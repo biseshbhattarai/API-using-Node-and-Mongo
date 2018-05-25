3eeb4fa8c4f37b01a737831ef25882443b08bb2a
bisesh <biseshbhattarai5@gmail.com>
2018-05-25 11:44:11 +0545

    First Commit

---
+++ app.js
@@ -0,0 +1,115 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+
+// var logger = function(req, res, next){
+// 	console.log("logging");
+// 	next();
+// }
+
+// app.use(logger);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+app.get('/', (req, res) => {
+	res.send('Please use /api/books or /api/genres');
+});
+
+app.get('/api/genres', (req, res) => {
+	Genre.getGenres((err, genres) => {
+		if(err){
+			throw err;
+		}
+		res.json(genres);
+	});
+});
+
+app.post('/api/genres', (req, res) => {
+	var genre = req.body;
+	Genre.addGenre(genre, (err, genre) => {
+		if(err){
+			throw err;
+		}
+		res.json(genre);
+	});
+});
+
+app.put('/api/genres/:_id', (req, res) => {
+	var id = req.params._id;
+	var genre = req.body;
+	Genre.updateGenre(id, genre, {}, (err, genre) => {
+		if(err){
+			throw err;
+		}
+		res.json(genre);
+	});
+});
+
+app.delete('/api/genres/:_id', (req, res) => {
+	var id = req.params._id;
+	Genre.DeleteGenre(id, (err, genre) => {
+		if(err){
+			throw err;
+		}
+		res.json(genre);
+	});
+});
+
+app.get('/api/books', (req, res) => {
+	Book.getBooks((err, books) => {
+		if(err){
+			throw err;
+		}
+		res.json(books);
+	});
+});
+
+app.get('/api/books/:_id', (req, res) => {
+	Book.getBookById(req.params._id, (err, book) => {
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
+});
+
+app.post('/api/books', (req, res) => {
+	var book = req.body;
+	Book.addBook(book, (err, book) => {
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
+});
+
+app.put('/api/books/:_id', (req, res) => {
+	var id = req.params._id;
+	var book = req.body;
+	Book.updateBook(id, book, {}, (err, book) => {
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
+});
+
+app.delete('/api/books/:_id', (req, res) => {
+	var id = req.params._id;
+	Book.DeleteBook(id, (err, book) => {
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
+});
+
+app.listen(3000, ()=>{
+	console.log('Server started on port 3000...')
+})
---
+++ models/genre.js
@@ -0,0 +1,45 @@
+
+
+var mongoose = require('mongoose');
+
+
+//Genre Schema
+var genreSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required: true
+    },
+    create_date:{
+        type:Date,
+        default:Date.now
+    }
+});
+
+var Genre = module.exports = mongoose.model('Genre', genreSchema)
+
+module.exports.getGenres = function(callback, limit){
+    Genre.find(callback).limit(limit);
+}
+
+//Add Genre
+
+module.exports.addGenre = function(genre, callback){
+    Genre.create(genre, callback);
+}
+
+
+
+module.exports.updateGenre = function(id, genre,options, callback){
+    var query = {_id:id};
+    var update = {
+        name:genre.name
+    }
+    Genre.findOneAndUpdate(query,update,options,genre, callback);
+}
+
+
+module.exports.Deletegenre = function(id, callback){
+    var query = {_id:id};
+    Genre.remove(query, callback);
+}
+
